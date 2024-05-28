792bc1dfbd5ebc04fb37f54eacf0608938676760
mohammadthariq@26 <thariq.md.ans@gmail.com>
2024-05-28 18:29:49 +0530

    Add Fav Icon

--- src/common/PageHeader/index.jsx
+++ src/common/PageHeader/index.jsx
@@ -5,6 +5,7 @@ export const PageHeader = ({ title }) => {
   const defaultTitle = "Shopo Admin";
   return (
     <Head>
+      <link rel="icon" href="/assets/img/favicon.ico" sizes="any" />
       <title>{getTitleFromPath(title) || defaultTitle}</title>
     </Head>
   );
