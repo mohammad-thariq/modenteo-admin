46b85e03fa8a467f433792755e2ebfa1cd6f34d1
Gomathy <gomathy1023@gmail.com>
2024-06-03 15:15:58 +0530

    env changes

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const BaseUrls = {
-  // BASE_URL: "https://api-modenteo.onrender.com/v1",
-  BASE_URL: "http://localhost:5000/v1",
+  BASE_URL: "https://api-modenteo.onrender.com/v1",
+  // BASE_URL: "http://localhost:3000/v1",
   IMAGE_URL: "https://api-modenteo.onrender.com",
   TINYMCE_API_KEY: "4kw1y7zv5bk17spvtze9tzm0gqxrgpncajdp9pv1o777mhpe",
 };
