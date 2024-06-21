efd679c3c6a970ec122a5e712dd2bc8df2303e42
Gomathy <gomathy1023@gmail.com>
2024-06-21 21:13:16 +0530

    Revert Chagnes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-const withTM = require('next-transpile-modules')(['react-s3']); // Add this line
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withTM({ // Wrap your existing config with withTM
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["modenteo-file.s3.eu-north-1.amazonaws.com"],
   },
-});
+};
 
 export default nextConfig;
