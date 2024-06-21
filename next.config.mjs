5833ef95b93fc278f235c14f2a59b375f55b78fb
Gomathy <gomathy1023@gmail.com>
2024-06-21 21:10:15 +0530

    Changes is next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
+const withTM = require('next-transpile-modules')(['react-s3']); // Add this line
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withTM({ // Wrap your existing config with withTM
   reactStrictMode: true,
   images: {
     domains: ["modenteo-file.s3.eu-north-1.amazonaws.com"],
   },
-};
+});
 
 export default nextConfig;
