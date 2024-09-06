3c64603cfadd89218cbfe83ead5fc713112a6e57
mohammad-thariq <thariq.dev@gmail.com>
2024-09-06 21:58:58 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const BaseUrls = {
-  BASE_URL: "https://16.171.249.99/v1",
+  BASE_URL: "http://54.253.196.111:3000/v1",
   // BASE_URL: "http://localhost:3000/v1",
   TINYMCE_API_KEY: "mzbuyoz9s6ud87oir4nh1xxo6fxnuqlpqbs5z0h2mo0qllr3",
   AWS_ACCESS_KEY_ID: "AKIAYS2NU2MNJ66GU2PF",
